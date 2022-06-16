32b843d2716e31f4a6054ec3aa521e9160729ed5
LoveToSpooch <love2spooch@mail.ru>
2022-06-16 22:48:12 +0300

    Lesson-2

---
+++ app/src/main/java/cheers/lovetospooch/appnews/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,19 @@
+package cheers.lovetospooch.appnews.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import cheers.lovetospooch.appnews.R
+
+class FavoriteFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+}
---
+++ app/src/main/java/cheers/lovetospooch/appnews/ui/splash/SplashFragment.kt
@@ -0,0 +1,6 @@
+package cheers.lovetospooch.appnews.ui.splash
+
+import androidx.fragment.app.Fragment
+
+class SplashFragment: Fragment() {
+}
